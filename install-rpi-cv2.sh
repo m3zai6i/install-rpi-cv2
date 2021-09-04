72c7dd465ce06080a0c79f694c09e4294e1df373
Shahzaib <51841651+m3zai6i@users.noreply.github.com>
2021-09-04 13:54:05 +0500

    Update install-rpi-cv2.sh

--- install-rpi-cv2.sh
+++ install-rpi-cv2.sh
@@ -6,4 +6,4 @@ sudo apt install libgtk-3-dev libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
 sudo apt install libatlas-base-dev liblapacke-dev gfortran
 sudo apt install libhdf5-dev libhdf5-103
 sudo apt install python3-dev python3-pip python3-numpy
-pip3 install opencv-python
+pip3 install opencv-python==4.4.0.46
